e8eff221b5ade99241acbf61b16c1cb91693e7f5
Peter Marek <peter.marek@stfc.ac.uk>
2019-11-08 12:45:16 +0000

    open_mfdataset

--- exercise.py
+++ exercise.py
@@ -2,6 +2,12 @@ from pywps import Process, LiteralInput, LiteralOutput, UOM
 from pywps import ComplexInput, ComplexOutput
 from pywps.app.Common import Metadata
 from pywps import Format, FORMATS
+from pywps.exceptions import InvalidParameterValue
+import xarray as xr
+
+
+import glob
+import os
 
 import logging
 LOGGER = logging.getLogger("PYWPS")
@@ -27,43 +33,29 @@ class Exercise(Process):
                          data_type='float',
                          default=90,
                          min_occurs=1),
-            LiteralInput('pr', 'Precipitation',
-                         abstract='at surface; includes both liquid and solid phases from all types of clouds (both large-scale and convective)',
-                         uoms='kg m-2 s-1',
-                         data_type='float'),
-            LiteralInput('tas', 'Near-Surface Air Temperature',
-                         uoms='K',
-                         data_type='float'),
-            LiteralInput('tasmax', 'Daily Maximum Near-Surface Air Temperature',
-                         uoms='K',
-                         data_type='float'),
-            LiteralInput('tasmin', 'Daily Minimum Near-Surface Air Temperature',
-                         uoms='K',
-                         data_type='float'),
-            LiteralInput('uas', 'Eastward Near-Surface Wind',
-                         uoms='m s-1',
-                         data_type='float'), 
-            LiteralInput('vas', 'Northward Near-Surface Wind',
-                         uoms='m s-1',
-                         data_type='float'),              
+            LiteralInput('variable', 'Variable',
+                         abstract='Choose a variable like vas (northward near-Surface wind).',
+                         data_type='string',
+                         allowed_values=['pr', 'tas', 'tasmax', 'tasmin', 'vas', 'uas'],
+                         default='tas'),
             LiteralInput('model', 'Model',
                          abstract='Choose a model like HadGEM2-ES.',
                          data_type='string',
                          allowed_values=['HadGEM2-ES',
                                          'HadCM3',
-                                         'GFDL-CM2p1'],
-                         default='HadGEM2-ES'),
-            LiteralInput('variable', 'Variable',
-                         abstract='Choose a variable like tas (temperatue at surface).',
-                         data_type='float',
-                         allowed_values=['', ''],
-                         default='tas')]
+                                         'GFDL-CM2p1',
+                                         'bcc-csm1-1-m',
+                                         'bcc-csm1-1',
+                                         'BNU-ESM',
+                                         ],
+                         default='HadCM3'),
+                         ]
         outputs = [
             ComplexOutput('output', 'NetCDF file',
                           abstract='A single NetCDF file.',
                           as_reference=True,
                           supported_formats=[FORMATS.NETCDF]),]
-            
+
 
         super(Exercise, self).__init__(
             self._handler,
@@ -86,7 +78,23 @@ class Exercise(Process):
 
     @staticmethod
     def _handler(request, response):
-        LOGGER.info("say hello")
-        response.outputs['output'].data = 'Hello ' + request.inputs['name'][0].data
-        response.outputs['output'].uom = UOM('unity')
+        model = request.inputs['model'][0].data
+        variable = request.inputs['variable'][0].data
+        nc_files_path = f'badc/cmip5/data/cmip5/output1/MOHC/{model}/rcp45/mon/atmos/Amon/r1i1p1/latest/{variable}/'
+        file_path = os.path.join(os.environ.get('HOME'), nc_files_path)
+        files = glob.glob(file_path + '*.nc')
+        ds = xr.open_mfdataset(files)
+
+
+        # Throw manually with temporary bbox solution
+        if request.inputs['min_lon'][0].data < -180:
+            raise InvalidParameterValue('Minimum longitude input cannot be below -180')
+        if request.inputs['max_lon'][0].data > 180:
+            raise InvalidParameterValue('Maximum longitude input cannot be above 180')
+        if request.inputs['min_lat'][0].data < -90:
+            raise InvalidParameterValue('Minimum latitude input cannot be below -90')
+        if request.inputs['max_lat'][0].data > 90:
+            raise InvalidParameterValue('Minimum latitude input cannot be above 90')
+
+
         return response
